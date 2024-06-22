15392bf3096e090db8235a92cb4b0222a503c65d
dpl0xr <dombeardown@gmail.com>
2024-06-22 11:59:15 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DealChadAI from './components/DealChadAI';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <DealChadAI />
+    </div>
+  );
+};
+
+export default App;
